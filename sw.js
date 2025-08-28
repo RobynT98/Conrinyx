9aa59c620a326aca083fe4c1dd54a994a13a3c97
RobynT98 <50335159+RobynT98@users.noreply.github.com>
2025-08-28 17:32:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,62 +1,54 @@
-// sw.js – ConriNyx Scenarioindex
-const CACHE_NAME = "conrinyx-cache-v5";
-const FILES_TO_CACHE = [
-  "/Conrinyx-senarie.index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/sw.js"
-];
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-// Install – lägg filer i cache
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+const CACHE = "pwabuilder-offline-page";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Activate – ta bort gamla cacheversioner
-self.addEventListener("activate", (event) => {
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
-  self.clients.claim();
 });
 
-// Fetch - network first för html, cache first för övriga
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    // För sidor (HTML) – försök nätverk först
-    event.respondWith(
-      fetch(event.request).catch(() =>
-        caches.match("/Conrinyx-senarie.index.html") // ✅ rätt filnamn
-      )
-    );
-  } else {
-    // För bilder, js, css etc – kolla cache först
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return (
-          response ||
-          fetch(event.request).then((resp) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, resp.clone());
-              return resp;
-            });
-          })
-        );
-      })
-    );
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
   }
 });
